750dd28beb77fce3137cc768b93cbd4bd65cefb7
ecirish <ecirish@gmail.com>
2023-07-21 14:29:51 -0400

    rearrange layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,42 @@
 <template lang="pug">
-h1 Hello World
+v-app
+  //- v-app-bar(
+  //-   flat
+  //-   :color="lgAndUp ? 'background' : 'primary'"
+  //-   floating
+  //- )
+  //-   v-app-bar-title
+  //-   v-spacer
+  //-   v-btn(
+  //-     v-if="user"
+  //-     icon="mdi-bell-outline"
+  //-   )
+  //-   v-menu(
+  //-     v-if="user"
+  //-     location="bottom"
+  //-   )
+  //-     template(v-slot:activator="{ props }")
+  //-       v-avatar.mr-4.ml-2.clickable(
+  //-         v-bind="props"
+  //-         size="32px"
+  //-         color="info"
+  //-       ) {{ user._initials }}
+
+  //-     v-list(min-width="150")
+  //-       v-list-item(
+  //-         to="/account"
+  //-       )
+  //-         v-list-item-title Account
+  //-       v-list-item(@click="runLogout")
+  //-         v-list-item-title Logout
+  v-main
+    NuxtPage
+  Footer
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+</style>
